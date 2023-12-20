b9c8d8be7dca3c441e5e45c40948f1a2da792a5e
apalebluedot9283 <apalebluedot9283@gmail.com>
2023-12-20 16:47:12 +0900

    10 week

---
+++ smm_object.c
@@ -0,0 +1,64 @@
+//
+//  smm_node.c Data encapsulation 방식으로 노드 데이터 저장하는 곳, 데이터 원본을 감싸서 감추는 프로그래밍 방식, main.c에 제공되는 함수로만 데이터 접근 가능 
+//  SMMarble
+//  smm_object.c 객체 (보드칸-노드, 음식카드, 축제카드, 수강 이력  및 성적) 구조체 생성 및 분석 
+//  Created by Juyeop Kim on 2023/11/05.
+//
+
+#include "smm_common.h"
+#include "smm_object.h"
+#include <string.h>
+
+#define MAX_NODETYPE    7
+#define MAX_GRADE       9
+#define MAX_NODE        100
+
+
+static char smmObj_name[MAX_NODE][MAX_CHARNAME]; //해당 문자열 배열 생성, 문자열을 반환하는 API 함수를 smm_object.c에 구현 
+static int smmObj_type[MAX_NODE];
+static int smmObj_credit[MAX_NODE];
+static int smmObj_energy[MAX_NODE];
+static int smmObj_noNode=0;
+
+
+
+//object generation
+void smmObj_genNode(char* name, int type, int credit, int energy)
+{
+    strcpy(smmObj_name[smmObj_noNode], name);
+    smmObj_type[smmObj_noNode] = type;
+    smmObj_credit[smmObj_noNode] = credit;
+    smmObj_energy[smmObj_noNode] = energy;
+
+    smmObj_noNode++;
+}
+
+char* smmObj_getNodeName(int node_nr)
+{
+    return smmObj_name[smmObj_noNode];
+}
+
+int smmObj_getNodeType(int node_nr)
+{
+    return smmObj_type[node_nr];
+}
+
+#if 0
+
+
+//member retrieving
+
+
+
+//element to string
+char* smmObj_getNodeName(smmNode_e type)
+{
+    return smmNodeName[type];
+}
+
+char* smmObj_getGradeName(smmGrade_e grade)
+{
+    return smmGradeName[grade];
+}
+
+#endif
